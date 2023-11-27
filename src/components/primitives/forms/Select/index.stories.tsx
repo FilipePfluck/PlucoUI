780268350ba59fe2c2ab66b4b4a2596832f09abf
Filipe Pfluck <filipepfluckdev@gmail.com>
2023-11-27 16:36:23 -0300

    feat: add props to select

--- src/components/primitives/forms/Select/index.stories.tsx
+++ src/components/primitives/forms/Select/index.stories.tsx
@@ -9,6 +9,90 @@ const meta: Meta<typeof Select> = {
 export default meta
 type Story = StoryObj<typeof Select>
 
+const itemsValues = [
+  'apple',
+  'banana',
+  'blueberry',
+  'grapes',
+  'pineapple',
+  'aubergine',
+  'broccoli',
+  'carrot',
+  'beef',
+  'chicken',
+  'lamb',
+  'pork',
+]
+
+const items = [
+  {
+    groupLabel: 'Fruits',
+    id: 'fruits',
+    items: [
+      {
+        label: 'Apple',
+        value: 'apple',
+      },
+      {
+        label: 'Banana',
+        value: 'banana',
+      },
+      {
+        label: 'Blueberry',
+        value: 'blueberry',
+      },
+      {
+        label: 'Grapes',
+        value: 'grapes',
+      },
+      {
+        label: 'Pineapple',
+        value: 'pineapple',
+      },
+    ],
+  },
+  {
+    groupLabel: 'Vegetables',
+    id: 'vieggies',
+    items: [
+      {
+        label: 'Aubergine',
+        value: 'aubergine',
+      },
+      {
+        label: 'Broccoli',
+        value: 'broccoli',
+      },
+      {
+        label: 'Carrot',
+        value: 'carrot',
+      },
+    ],
+  },
+  {
+    groupLabel: 'Meats',
+    id: 'meats',
+    items: [
+      {
+        label: 'Beef',
+        value: 'beef',
+      },
+      {
+        label: 'Chicken',
+        value: 'chicken',
+      },
+      {
+        label: 'Lamb',
+        value: 'lamb',
+      },
+      {
+        label: 'Pork',
+        value: 'pork',
+      },
+    ],
+  },
+]
+
 export const Default: Story = {
-  render: () => <Select />,
+  render: () => <Select items={items} label="food" itemsValues={itemsValues} />,
 }
--- src/components/primitives/forms/Select/index.tsx
+++ src/components/primitives/forms/Select/index.tsx
@@ -2,25 +2,25 @@ import { ChevronsUpDown } from 'lucide-react'
 import * as S from './styles'
 import { Portal } from '@ark-ui/react'
 
-export const Select = () => {
+interface SelectItemProps {
+  label: string
+  value: string
+}
+
+interface SelectProps {
+  items: {
+    groupLabel?: string
+    id: string
+    items: SelectItemProps[]
+  }[]
+  itemsValues: string[]
+  label?: string
+}
+
+export const Select = ({ items, itemsValues, label }: SelectProps) => {
   return (
-    <S.Root
-      items={[
-        'apple',
-        'banana',
-        'blueberry',
-        'grapes',
-        'pineapple',
-        'aubergine',
-        'broccoli',
-        'carrot',
-        'beef',
-        'chicken',
-        'lamb',
-        'pork',
-      ]}
-    >
-      <S.Label>Fruits</S.Label>
+    <S.Root items={itemsValues}>
+      {label && <S.Label>{label}</S.Label>}
       <S.Control>
         <S.Trigger>
           <S.ValueText />
@@ -32,55 +32,18 @@ export const Select = () => {
       <Portal>
         <S.Positioner>
           <S.Content>
-            <S.ItemGroup id="fruits">
-              <S.ItemGroupLabel htmlFor="fruits">Fruits</S.ItemGroupLabel>
-              <S.Item item="apple">
-                <S.ItemText>Apple</S.ItemText>
-              </S.Item>
-              <S.Item item="banana">
-                <S.ItemText>Banana</S.ItemText>
-              </S.Item>
-              <S.Item item="blueberry">
-                <S.ItemText>Blueberry</S.ItemText>
-              </S.Item>
-              <S.Item item="grapes">
-                <S.ItemText>Grapes</S.ItemText>
-              </S.Item>
-              <S.Item item="pineapple">
-                <S.ItemText>Pineapple</S.ItemText>
-              </S.Item>
-            </S.ItemGroup>
-
-            <S.ItemGroup id="vegetables">
-              <S.ItemGroupLabel htmlFor="vegetables">
-                Vegetables
-              </S.ItemGroupLabel>
-              <S.Item item="aubergine">
-                <S.ItemText>Aubergine</S.ItemText>
-              </S.Item>
-              <S.Item item="broccoli">
-                <S.ItemText>Broccoli</S.ItemText>
-              </S.Item>
-              <S.Item item="carrot">
-                <S.ItemText>Carrot</S.ItemText>
-              </S.Item>
-            </S.ItemGroup>
-
-            <S.ItemGroup id="meats">
-              <S.ItemGroupLabel htmlFor="meats">Meats</S.ItemGroupLabel>
-              <S.Item item="beef">
-                <S.ItemText>Beef</S.ItemText>
-              </S.Item>
-              <S.Item item="chicken">
-                <S.ItemText>Chicken</S.ItemText>
-              </S.Item>
-              <S.Item item="lamb">
-                <S.ItemText>Lamb</S.ItemText>
-              </S.Item>
-              <S.Item item="pork">
-                <S.ItemText>Pork</S.ItemText>
-              </S.Item>
-            </S.ItemGroup>
+            {items.map(({ id, items, groupLabel }) => (
+              <S.ItemGroup id={id} key={id}>
+                {groupLabel && (
+                  <S.ItemGroupLabel htmlFor={id}>{groupLabel}</S.ItemGroupLabel>
+                )}
+                {items.map(({ label, value }) => (
+                  <S.Item item={value} key={value}>
+                    <S.ItemText>{label}</S.ItemText>
+                  </S.Item>
+                ))}
+              </S.ItemGroup>
+            ))}
           </S.Content>
         </S.Positioner>
       </Portal>
