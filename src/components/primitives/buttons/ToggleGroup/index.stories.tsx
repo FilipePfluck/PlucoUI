810db431e1098a6a02dbfc6006a8d0fe0a6135c7
Filipe Pfluck <filipepfluckdev@gmail.com>
2024-02-21 16:13:25 -0300

    feat: add tests for toggle group

--- src/components/primitives/buttons/ToggleGroup/index.stories.tsx
+++ src/components/primitives/buttons/ToggleGroup/index.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent, expect } from '@storybook/test'
+
 import {
   AlignCenter,
   AlignLeft,
@@ -14,6 +16,24 @@ import { Flex } from '@/styled-system/jsx'
 const meta: Meta<typeof ToggleGroup> = {
   component: ToggleGroup,
 }
+
+const testItems: ToggleProps[] = [
+  {
+    id: 'first',
+    'aria-label': 'first item',
+    children: '1',
+  },
+  {
+    id: 'second',
+    'aria-label': 'second item',
+    children: '2',
+  },
+  {
+    id: 'third',
+    'aria-label': 'third item',
+    children: '3',
+  },
+]
 
 const formatItems: ToggleProps[] = [
   {
@@ -54,6 +74,15 @@ const alignItems: ToggleProps[] = [
 export default meta
 type Story = StoryObj<typeof ToggleGroup>
 
+export const Horizontal: Story = {
+  render: () => (
+    <Flex gap="4">
+      <ToggleGroup items={formatItems} multiple orientation="horizontal" />
+      <ToggleGroup items={alignItems} orientation="horizontal" />
+    </Flex>
+  ),
+}
+
 export const Vertical: Story = {
   render: () => (
     <Flex direction="column" gap="4">
@@ -63,11 +92,60 @@ export const Vertical: Story = {
   ),
 }
 
-export const Horizontal: Story = {
-  render: () => (
-    <Flex gap="4">
-      <ToggleGroup items={formatItems} multiple orientation="horizontal" />
-      <ToggleGroup items={alignItems} orientation="horizontal" />
-    </Flex>
-  ),
+export const Single: Story = {
+  args: {
+    items: testItems,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    // the first item should not start checked
+    const firstItem = canvas.getByLabelText('first item')
+    await expect(firstItem).not.toBeChecked()
+
+    // click the first element, it should be checked now
+    await userEvent.click(firstItem)
+    await expect(firstItem).toBeChecked()
+
+    // the second item should not start checked
+    const secondItem = canvas.getByLabelText('second item')
+    await expect(secondItem).not.toBeChecked()
+
+    // click the second element, it should be checked now
+    await userEvent.click(secondItem)
+    await expect(secondItem).toBeChecked()
+
+    // the first item shouldn't be checked anymore
+    await expect(firstItem).not.toBeChecked()
+  },
+}
+
+export const Multiple: Story = {
+  args: {
+    items: testItems,
+    multiple: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    // the first item should not start checked
+    const firstItem = canvas.getByLabelText('first item')
+    await expect(firstItem).toHaveAttribute('data-state', 'off')
+
+    // click the first element, it should be checked now
+    await userEvent.click(firstItem)
+    await expect(firstItem).toHaveAttribute('data-state', 'on')
+
+    // the second item should not start checked
+    const secondItem = canvas.getByLabelText('second item')
+    await expect(secondItem).toHaveAttribute('data-state', 'off')
+
+    // click the second element, it should be checked now
+    await userEvent.click(secondItem)
+    await expect(secondItem).toHaveAttribute('data-state', 'on')
+
+    // the first item should still be checked
+    // (because this toggle group allows multiple checks)
+    await expect(firstItem).toHaveAttribute('data-state', 'on')
+  },
 }
