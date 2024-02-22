073c5d60bb264ffd783887c8aa83d77851386b13
Filipe Pfluck <filipepfluckdev@gmail.com>
2024-02-22 09:46:57 -0300

    feat: accordion tests

--- src/components/primitives/disclosure/Accordion/index.stories.tsx
+++ src/components/primitives/disclosure/Accordion/index.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent, expect, waitFor } from '@storybook/test'
 
 import { Accordion } from './index'
-import { css } from '@/styled-system/css'
 
 const meta: Meta<typeof Accordion> = {
   component: Accordion,
@@ -29,85 +29,108 @@ const data = [
   },
 ]
 
-const data2 = [
-  {
-    id: 'item-1',
-    title: 'Is it accessible?',
-    content: 'Yes. It adheres to the WAI-ARIA design pattern.',
+export const AccordionStory: Story = {
+  args: {
+    items: data,
+    multiple: false,
+    collapsible: false,
   },
-  {
-    id: 'item-2',
-    title: 'Is it styled?',
-    content: 'Yes, it comes with beautifull styles by default.',
-  },
-  {
-    id: 'item-3',
-    title: 'Is it animated?',
-    content: 'Yes, it comes with beautifull animations by default.',
-  },
-  {
-    id: 'item-4',
-    title: 'Can multiple itens be open?',
-    content: 'Yes, it can.',
-  },
-]
+}
 
 export const Single: Story = {
-  render: () => (
-    <div
-      className={css({
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '4',
-      })}
-    >
-      <Accordion items={data} />
-    </div>
-  ),
+  args: {
+    items: data,
+    multiple: false,
+    collapsible: false,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const firstItem = canvas.getByText(
+      'Yes. It adheres to the WAI-ARIA design pattern.',
+    )
+
+    const secondItem = canvas.getByText(
+      'Yes, it comes with beautifull styles by default.',
+    )
+
+    await waitFor(() => expect(firstItem).not.toBeVisible())
+    await waitFor(() => expect(secondItem).not.toBeVisible())
+
+    const [firstItemTrigger, secondItemTrigger] = canvas.getAllByRole('button')
+
+    // click the first trigger. The first item should now be visible
+    await userEvent.click(firstItemTrigger)
+    await expect(firstItem).toBeVisible()
+
+    // click the second trigger.
+    // The second item should now be visible
+    // and the first one should not
+
+    await userEvent.click(secondItemTrigger)
+    await waitFor(() => expect(secondItem).toBeVisible())
+    await waitFor(() => expect(firstItem).not.toBeVisible())
+  },
 }
 
 export const Multiple: Story = {
-  render: () => (
-    <div
-      className={css({
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '4',
-      })}
-    >
-      <Accordion multiple items={data2} />
-    </div>
-  ),
-}
+  args: {
+    items: data,
+    multiple: true,
+    collapsible: false,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
 
-export const Collapsible: Story = {
-  render: () => (
-    <div
-      className={css({
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '4',
-      })}
-    >
-      <Accordion multiple collapsible items={data2} />
-    </div>
-  ),
+    const firstItem = canvas.getByText(
+      'Yes. It adheres to the WAI-ARIA design pattern.',
+    )
+
+    const secondItem = canvas.getByText(
+      'Yes, it comes with beautifull styles by default.',
+    )
+
+    await waitFor(() => expect(firstItem).not.toBeVisible())
+    await waitFor(() => expect(secondItem).not.toBeVisible())
+
+    const [firstItemTrigger, secondItemTrigger] = canvas.getAllByRole('button')
+
+    // click the first trigger. The first item should now be visible
+    await userEvent.click(firstItemTrigger)
+    await expect(firstItem).toBeVisible()
+
+    // click the second trigger.
+    // The second item should now be visible
+    // and the first one too
+
+    await userEvent.click(secondItemTrigger)
+    await waitFor(() => expect(secondItem).toBeVisible())
+    await waitFor(() => expect(firstItem).toBeVisible())
+  },
 }
 
 export const DefaultOpen: Story = {
-  render: () => (
-    <div
-      className={css({
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '4',
-      })}
-    >
-      <Accordion collapsible defaultValue={['item-1']} items={data2} />
-    </div>
-  ),
+  args: {
+    items: data,
+    multiple: true,
+    collapsible: true,
+    defaultValue: ['item-1'],
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const firstItem = canvas.getByText(
+      'Yes. It adheres to the WAI-ARIA design pattern.',
+    )
+
+    const secondItem = canvas.getByText(
+      'Yes, it comes with beautifull styles by default.',
+    )
+
+    // the first Item should be visible by default
+    // the second item should not be visible by default
+
+    await waitFor(() => expect(firstItem).toBeVisible())
+    await waitFor(() => expect(secondItem).not.toBeVisible())
+  },
 }
