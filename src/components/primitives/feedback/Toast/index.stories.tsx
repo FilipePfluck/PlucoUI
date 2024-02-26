45bc818a844987aa783b8748951242ba445c54f2
Filipe Pfluck <filipepfluckdev@gmail.com>
2024-02-26 18:24:35 -0300

    feat: toast tests

--- src/components/primitives/feedback/Toast/index.stories.tsx
+++ src/components/primitives/feedback/Toast/index.stories.tsx
@@ -1,4 +1,6 @@
 import { Button } from '../../buttons/Button'
+import { within, userEvent, expect, waitFor } from '@storybook/test'
+
 import { Toaster, toast } from './index'
 
 import type { Meta, StoryObj } from '@storybook/react'
@@ -27,4 +29,22 @@ export const Primary: Story = {
       <Toaster />
     </>
   ),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const button = await canvas.findByRole('button')
+
+    await userEvent.click(button)
+
+    await waitFor(async () => {
+      // should open a toast when clicking the trigger
+      const toast = await canvas.findByRole('status')
+      expect(toast).toBeVisible()
+
+      // should close the toast when clicking the close button
+      const closeButton = await within(toast).findByRole('button')
+      await userEvent.click(closeButton)
+      expect(toast).not.toBeVisible()
+    })
+  },
 }
