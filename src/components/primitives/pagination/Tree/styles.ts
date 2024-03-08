4451d821dfbd9cfe9a28cb7b2454f341e0de0e7d
Filipe Pfluck <filipepfluckdev@gmail.com>
2024-03-08 18:27:50 -0300

    feat: tree view wip

---
+++ src/components/primitives/pagination/Tree/styles.ts
@@ -0,0 +1,22 @@
+import { styled } from '@/styled-system/jsx'
+import { TreeView } from '@ark-ui/react'
+
+export const Root = styled(TreeView.Root, {})
+
+export const Tree = styled(TreeView.Tree, {})
+
+export const Item = styled(TreeView.Item, {})
+
+export const ItemText = styled(TreeView.ItemText, {})
+
+export const ItemIndicator = styled(TreeView.ItemIndicator, {})
+
+export const Branch = styled(TreeView.Branch, {})
+
+export const BranchControl = styled(TreeView.BranchControl, {})
+
+export const BranchText = styled(TreeView.BranchText, {})
+
+export const BranchIndicator = styled(TreeView.BranchIndicator, {})
+
+export const BranchContent = styled(TreeView.BranchContent, {})
