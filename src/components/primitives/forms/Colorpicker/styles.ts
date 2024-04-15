74450f5ca92533128ce99b3cafbe61ef29bbe919
Filipe Pfluck <filipepfluckdev@gmail.com>
2024-04-15 16:50:30 -0300

    feat: colorpicker

---
+++ src/components/primitives/forms/Colorpicker/styles.ts
@@ -0,0 +1,80 @@
+import { styled } from '@/styled-system/jsx'
+import { ColorPicker } from '@ark-ui/react'
+
+export const Root = styled(ColorPicker.Root, {
+  base: {},
+})
+
+export const Label = styled(ColorPicker.Label, {
+  base: {},
+})
+
+export const Control = styled(ColorPicker.Control, {
+  base: {},
+})
+
+export const ChannelInput = styled(ColorPicker.ChannelInput, {
+  base: {},
+})
+
+export const ValueText = styled(ColorPicker.ValueText, { base: {} })
+
+export const Trigger = styled(ColorPicker.Trigger, {
+  base: {},
+})
+
+export const TransparencyGrid = styled(ColorPicker.TransparencyGrid, {
+  base: {},
+})
+
+export const Swatch = styled(ColorPicker.Swatch, {
+  base: {},
+})
+
+export const Positioner = styled(ColorPicker.Positioner, {
+  base: {},
+})
+
+export const Content = styled(ColorPicker.Content, {
+  base: {},
+})
+
+export const Area = styled(ColorPicker.Area, {
+  base: {},
+})
+
+export const AreaBackground = styled(ColorPicker.AreaThumb, {
+  base: {},
+})
+
+export const ChannelSlider = styled(ColorPicker.ChannelSlider, {
+  base: {},
+})
+
+export const ChannelSliderTrack = styled(ColorPicker.ChannelSliderTrack, {
+  base: {},
+})
+
+export const ChannelSliderThumb = styled(ColorPicker.ChannelSliderThumb, {
+  base: {},
+})
+
+export const SwatchGroup = styled(ColorPicker.SwatchGroup, {
+  base: {},
+})
+
+export const SwatchTrigger = styled(ColorPicker.SwatchTrigger, {
+  base: {},
+})
+
+export const SwatchIndicator = styled(ColorPicker.SwatchIndicator, {
+  base: {},
+})
+
+export const View = styled(ColorPicker.View, {
+  base: {},
+})
+
+export const EyeDropperTrigger = styled(ColorPicker.EyeDropperTrigger, {
+  base: {},
+})
