4d404357413bd54777d9441a19f5d560650b4bae
Filipe Pfluck <filipepfluckdev@gmail.com>
2024-05-24 18:50:00 -0300

    feat: new colors

--- panda/tokens/semantic.ts
+++ panda/tokens/semantic.ts
@@ -9,149 +9,125 @@ export const semanticTokens = defineSemanticTokens({
           _dark: '{colors.mauveDark.1}',
         },
       },
-      card: {
+      surface: {
         DEFAULT: {
+          value: {
+            base: '{colors.mauve.2}',
+            _dark: '{colors.mauveDark.2}',
+          },
+        },
+        hovered: {
           value: {
             base: '{colors.mauve.3}',
             _dark: '{colors.mauveDark.3}',
           },
         },
-        hovered: {
+        disabled: {
           value: {
-            base: '{colors.mauve.4}',
-            _dark: '{colors.mauveDark.4}',
+            base: '{colors.mauve.2}/40',
+            _dark: '{colors.mauveDark.2}/40',
           },
         },
       },
-      disabled: {
+      subtle: {
         value: {
-          base: '{colors.mauve.2}',
-          _dark: '{colors.mauveDark.2}',
-        },
-      },
-      brand: {
-        subtle: {
-          DEFAULT: {
-            value: {
-              base: '{colors.violet.1}',
-              _dark: '{colors.violetDark.1}',
-            },
-          },
-          hovered: {
-            value: {
-              base: '{colors.violet.2}',
-              _dark: '{colors.violetDark.2}',
-            },
-          },
-        },
-        selection: {
-          value: {
-            base: '{colors.violet.4}',
-            _dark: '{colors.violetDark.4}',
-          },
-        },
-        solid: {
-          DEFAULT: {
-            value: {
-              base: '{colors.violet.6}',
-              _dark: '{colors.violetDark.6}',
-            },
-          },
-          hovered: {
-            value: {
-              base: '{colors.violet.7}',
-              _dark: '{colors.violetDark.7}',
-            },
-          },
-          disabled: {
-            value: {
-              base: '{colors.violet.5}',
-              _dark: '{colors.violetDark.5}',
-            },
-          },
+          base: '{colors.mauve.6}/6',
+          _dark: '{colors.mauveDark.6}/6',
         },
       },
       danger: {
-        subtle: {
-          DEFAULT: {
-            value: {
-              base: '{colors.red.1}',
-              _dark: '{colors.redDark.1}',
-            },
-          },
-          hovered: {
-            value: {
-              base: '{colors.red.2}',
-              _dark: '{colors.redDark.2}',
-            },
-          },
-        },
-        selection: {
+        DEFAULT: {
           value: {
-            base: '{colors.red.4}',
-            _dark: '{colors.redDark.4}',
+            base: '{colors.red.5}',
+            _dark: '{colors.redDark.5}',
           },
         },
-        solid: {
-          DEFAULT: {
-            value: {
-              base: '{colors.red.6}',
-              _dark: '{colors.redDark.6}',
-            },
+        hovered: {
+          value: {
+            base: '{colors.red.6}',
+            _dark: '{colors.redDark.6}',
           },
-          hovered: {
-            value: {
-              base: '{colors.red.7}',
-              _dark: '{colors.redDark.7}',
-            },
+        },
+        disabled: {
+          value: {
+            base: '{colors.red.5}/40',
+            _dark: '{colors.redDark.5}/40',
           },
-          disabled: {
-            value: {
-              base: '{colors.red.5}',
-              _dark: '{colors.redDark.5}',
-            },
+        },
+        userSelect: {
+          value: {
+            base: '{colors.red.5}/20',
+            _dark: '{colors.redDark.5}/20',
+          },
+        },
+        subtle: {
+          value: {
+            base: '{colors.red.7}/6',
+            _dark: '{colors.redDark.7}/6',
           },
         },
       },
       success: {
-        subtle: {
-          DEFAULT: {
-            value: {
-              base: '{colors.green.1}',
-              _dark: '{colors.greenDark.1}',
-            },
-          },
-          hovered: {
-            value: {
-              base: '{colors.green.2}',
-              _dark: '{colors.greenDark.2}',
-            },
-          },
-        },
-        selection: {
+        DEFAULT: {
           value: {
-            base: '{colors.green.4}',
-            _dark: '{colors.greenDark.4}',
+            base: '{colors.green.5}',
+            _dark: '{colors.greenDark.5}',
           },
         },
-        solid: {
-          DEFAULT: {
-            value: {
-              base: '{colors.green.6}',
-              _dark: '{colors.greenDark.6}',
-            },
+        hovered: {
+          value: {
+            base: '{colors.green.6}',
+            _dark: '{colors.greenDark.6}',
           },
-          hovered: {
-            value: {
-              base: '{colors.green.7}',
-              _dark: '{colors.greenDark.7}',
-            },
+        },
+        disabled: {
+          value: {
+            base: '{colors.green.5}/40',
+            _dark: '{colors.greenDark.5}/40',
           },
-          disabled: {
-            value: {
-              base: '{colors.green.5}',
-              _dark: '{colors.greenDark.5}',
-            },
+        },
+        userSelect: {
+          value: {
+            base: '{colors.green.5}/20',
+            _dark: '{colors.greenDark.5}/20',
+          },
+        },
+        subtle: {
+          value: {
+            base: '{colors.green.7}/6',
+            _dark: '{colors.greenDark.7}/6',
+          },
+        },
+      },
+      brand: {
+        DEFAULT: {
+          value: {
+            base: '{colors.violet.5}',
+            _dark: '{colors.violetDark.5}',
+          },
+        },
+        hovered: {
+          value: {
+            base: '{colors.violet.6}',
+            _dark: '{colors.violetDark.6}',
+          },
+        },
+        disabled: {
+          value: {
+            base: '{colors.violet.5}/40',
+            _dark: '{colors.violetDark.5}/40',
+          },
+        },
+        userSelect: {
+          value: {
+            base: '{colors.violet.5}/20',
+            _dark: '{colors.violetDark.5}/20',
+          },
+        },
+        subtle: {
+          value: {
+            base: '{colors.violet.7}/6',
+            _dark: '{colors.violetDark.7}/6',
           },
         },
       },
@@ -159,11 +135,11 @@ export const semanticTokens = defineSemanticTokens({
     fg: {
       DEFAULT: {
         value: {
-          base: '{colors.mauve.9}',
-          _dark: '{colors.mauveDark.9}',
+          base: '{colors.mauve.6}',
+          _dark: '{colors.mauveDark.6}',
         },
       },
-      muted: {
+      important: {
         value: {
           base: '{colors.mauve.7}',
           _dark: '{colors.mauveDark.7}',
@@ -171,125 +147,94 @@ export const semanticTokens = defineSemanticTokens({
       },
       unimportant: {
         value: {
-          base: '{colors.mauve.8}',
-          _dark: '{colors.mauveDark.8}',
+          base: '{colors.mauve.5}',
+          _dark: '{colors.mauveDark.5}',
         },
       },
-      important: {
+      disabled: {
         value: {
-          base: '{colors.mauve.10}',
-          _dark: '{colors.mauveDark.10}',
-        },
-      },
-
-      brand: {
-        subtle: {
-          value: {
-            base: '{colors.violet.8}',
-            _dark: '{colors.violetDark.8}',
-          },
-        },
-        DEFAULT: {
-          value: {
-            base: '{colors.violet.9}',
-            _dark: '{colors.violetDark.9}',
-          },
-        },
-        important: {
-          value: {
-            base: '{colors.violet.10}',
-            _dark: '{colors.violetDark.10}',
-          },
+          base: '{colors.mauve.7}/40',
+          _dark: '{colors.mauveDark.7}/50',
         },
       },
       danger: {
-        subtle: {
-          value: {
-            base: '{colors.red.8}',
-            _dark: '{colors.redDark.8}',
-          },
-        },
-        DEFAULT: {
-          value: {
-            base: '{colors.red.9}',
-            _dark: '{colors.redDark.9}',
-          },
-        },
-        important: {
-          value: {
-            base: '{colors.red.10}',
-            _dark: '{colors.redDark.10}',
-          },
+        value: {
+          base: '{colors.red.7}',
+          _dark: '{colors.redDark.7}',
         },
       },
       success: {
-        subtle: {
-          value: {
-            base: '{colors.green.8}',
-            _dark: '{colors.greenDark.8}',
-          },
+        value: {
+          base: '{colors.green.7}',
+          _dark: '{colors.greenDark.7}',
         },
-        DEFAULT: {
-          value: {
-            base: '{colors.green.9}',
-            _dark: '{colors.greenDark.9}',
-          },
-        },
-        important: {
-          value: {
-            base: '{colors.green.10}',
-            _dark: '{colors.greenDark.10}',
-          },
+      },
+      brand: {
+        value: {
+          base: '{colors.violet.7}',
+          _dark: '{colors.violetDark.7}',
         },
       },
     },
     border: {
       DEFAULT: {
         value: {
-          base: '{colors.mauve.6}',
-          _dark: '{colors.mauveDark.6}',
+          base: '{colors.mauve.4}',
+          _dark: '{colors.mauveDark.4}',
+        },
+      },
+      subtle: {
+        value: {
+          base: '{colors.mauve.7}/12',
+          _dark: '{colors.mauveDark.7}/12',
         },
       },
       disabled: {
         value: {
-          base: '{colors.mauve.5}',
-          _dark: '{colors.mauveDark.5}',
+          base: '{colors.mauve.4}/40',
+          _dark: '{colors.mauveDark.4}/40',
         },
       },
-      brand: {
-        value: {
-          base: '{colors.violet.3}',
-          _dark: '{colors.violetDark.3}',
+      dander: {
+        DEFAULT: {
+          value: {
+            base: '{colors.red.5}',
+            _dark: '{colors.redDark.5}',
+          },
         },
-      },
-      danger: {
-        value: {
-          base: '{colors.red.3}',
-          _dark: '{colors.redDark.3}',
+        subtle: {
+          value: {
+            base: '{colors.red.7}/12',
+            _dark: '{colors.redDark.7}/12',
+          },
         },
       },
       success: {
-        value: {
-          base: '{colors.green.3}',
-          _dark: '{colors.greenDark.3}',
+        DEFAULT: {
+          value: {
+            base: '{colors.green.5}',
+            _dark: '{colors.greenDark.5}',
+          },
+        },
+        subtle: {
+          value: {
+            base: '{colors.green.7}/12',
+            _dark: '{colors.greenDark.7}/12',
+          },
         },
       },
-      ring: {
-        value: {
-          base: '{colors.violet.6}',
-          _dark: '{colors.violetDark.6}',
+      brand: {
+        DEFAULT: {
+          value: {
+            base: '{colors.violet.5}',
+            _dark: '{colors.violetDark.5}',
+          },
         },
-      },
-      focused: {
-        value: {
-          base: '{colors.violet.6}',
-          _dark: '{colors.violetDark.6}',
-        },
-      },
-      invalid: {
-        value: {
-          base: '{colors.red.6}',
-          _dark: '{colors.redDark.6}',
+        subtle: {
+          value: {
+            base: '{colors.violet.7}/12',
+            _dark: '{colors.violetDark.7}/12',
+          },
         },
       },
     },
