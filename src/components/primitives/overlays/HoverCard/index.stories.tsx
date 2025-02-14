721aa8c14475b547bca72500e811ce87ec928c47
Filipe Pfluck <filipepfluckdev@gmail.com>
2025-02-14 15:15:19 -0300

    feat: update hover card example

--- src/components/primitives/overlays/HoverCard/index.stories.tsx
+++ src/components/primitives/overlays/HoverCard/index.stories.tsx
@@ -2,14 +2,40 @@ import { HoverCard } from '@ark-ui/react'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { HoverCardContent } from '.'
+import { Avatar } from '../../media/Avatar'
+import { Flex } from '@/styled-system/jsx'
+import { Text } from '../../typography/Text'
+import Link from 'next/link'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const ExampleHoverCard = () => {
   return (
     <HoverCard.Root positioning={{ gutter: 24 }}>
-      <HoverCard.Trigger>Hover me</HoverCard.Trigger>
+      <HoverCard.Trigger>
+        <Avatar
+          name="Filipe Pfluck"
+          src="https://avatars.githubusercontent.com/u/62773200?v=4"
+        />
+      </HoverCard.Trigger>
       <HoverCardContent>
-        This is a HoverCard this is a Hover Card this is a Hover Card this is a
-        Hover Card this is a Hover Card
+        <Flex gap="4" align="center">
+          <Avatar
+            size="lg"
+            name="Filipe Pfluck"
+            src="https://avatars.githubusercontent.com/u/62773200?v=4"
+          />
+          <Flex direction="column" gap="2">
+            <Text textStyle="headingSm">Filipe Pfluck</Text>
+            <Flex gap="2">
+              <Link href="https://github.com/FilipePfluck">
+                <FaGithub />
+              </Link>
+              <Link href="https://www.linkedin.com/in/filipe-hebestreit-pfluck-9389a31a6/">
+                <FaLinkedin />
+              </Link>
+            </Flex>
+          </Flex>
+        </Flex>
       </HoverCardContent>
     </HoverCard.Root>
   )
